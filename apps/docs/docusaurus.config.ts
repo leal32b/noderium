32edaa8176ababd9d14526bf280ebf9a90e36dab
leal32b <leal32b@gmail.com>
2025-12-29 01:56:13 -0300

    chore: config docs github actions

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Noderium Docs',
+  tagline: 'Local-first note-taking app with bidirectional links and graph view. Built with Vite, Tauri, Rust, SolidJS and SQLite',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://leal32b.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/noderium/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'leal32b', // Usually your GitHub org/user name.
+  projectName: 'noderium', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch to deploy the documentation to.
+  trailingSlash: false, // Whether to add a trailing slash to the URLs.
 
   onBrokenLinks: 'throw',
 
