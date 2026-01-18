1e2666481291929e7d0d4fdfc9e694d1a1e38272
Fernando Oliveira <leal32b@hotmail.com>
2026-01-18 15:59:19 -0300

    refactor: remove index

--- apps/app/src/components/editor/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as MarkdownEditor } from "./MarkdownEditor";
-export type { MarkdownEditorProps, LineInfo, Viewport, ViewportState, FenceMatch } from "./types";
-export {
-  codeBlockDecorations,
-  hideMarkdownExceptCurrentLine,
-  markdownSemanticStyles,
-} from "./decorations";
-export { isEmptyListItem, handleEnterInList, listKeymap } from "./keybindings";
-export { editorTheme } from "./theme";
