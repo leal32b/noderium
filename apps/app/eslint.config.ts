77411ddadc64b3749d027dc69eb64486ab55baad
Fernando Oliveira <leal32b@hotmail.com>
2026-01-31 08:02:50 -0300

    chore: add eslint

---
+++ apps/app/eslint.config.ts
@@ -0,0 +1,45 @@
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import perfectionist from 'eslint-plugin-perfectionist'
+import solid from 'eslint-plugin-solid/configs/typescript'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+const __dirname = (import.meta as { dirname: string } & ImportMeta).dirname
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  solid,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname
+      }
+    }
+  },
+  stylistic.configs.customize({
+    commaDangle: 'never',
+    quotes: 'single',
+    semi: false
+  }),
+  perfectionist.configs['recommended-alphabetical'],
+  {
+    rules: {
+      '@stylistic/max-len': ['warn', { code: 100, ignoreStrings: true, ignoreUrls: true }],
+      'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }]
+    }
+  },
+  {
+    ignores: [
+      'coverage/',
+      'dist/',
+      'node_modules/',
+      'src-tauri/'
+    ]
+  }
+)
