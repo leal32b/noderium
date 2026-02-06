f2d15381d11c7b622b947b973f7bd5879c29169e
Fernando Oliveira <leal32b@hotmail.com>
2026-02-06 11:31:33 -0300

    chore: add daisyui

--- apps/app/src/index.tsx
+++ apps/app/src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 
 import '@/App.css'
--- apps/app/uno.config.ts
+++ apps/app/uno.config.ts
@@ -1,8 +1,13 @@
 import presetIcons from '@unocss/preset-icons'
+import { presetDaisy } from '@unscatty/unocss-preset-daisy'
 import { defineConfig, presetWind3 } from 'unocss'
 
 export default defineConfig({
-  presets: [presetWind3(), presetIcons()],
+  presets: [
+    presetWind3(),
+    presetDaisy(),
+    presetIcons()
+  ],
   theme: {
     colors: {
       main: 'var(--color-bg-main)'
