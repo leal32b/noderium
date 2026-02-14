ebd58f8f4ab8aeb21c582d2982321700947bf4d0
Fernando Oliveira <leal32b@hotmail.com>
2026-02-14 16:59:02 -0300

    refactor: adjust App

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,15 +1,18 @@
 import { MarkdownEditor } from '@/components/editor/MarkdownEditor'
+import { Drawer } from '@/components/layout/Drawer'
 import { Navbar } from '@/components/layout/Navbar'
-import { Workspace } from '@/components/layout/Workspace'
 
 function App() {
   return (
-    <main class="h-screen flex flex-col">
-      <Navbar />
-      <Workspace
-        center={<MarkdownEditor />}
-        left={<div class="p-4 pt-12">Left Sidebar</div>}
-        right={<div class="p-4 pt-12">Right Sidebar</div>}
+    <main>
+      <Drawer
+        content={(
+          <>
+            <Navbar />
+            <MarkdownEditor />
+          </>
+        )}
+        sideContent={(<></>)}
       />
     </main>
   )
